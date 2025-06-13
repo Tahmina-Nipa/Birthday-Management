f48d47287c9de21a81b470122711c71c3f75a42c
IT22058 <nipatahmina2@gmail.com>
2025-06-13 20:54:26 +0600

    Database.java

--- Database.java
+++ Database.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class Database {
     private static final String URL = "jdbc:mysql://localhost:3306/birthday_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "alamin";
+    private static final String PASSWORD = "nipa";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
